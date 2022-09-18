9410d80670afd4e1ea0354e5a987e2fa1cb86553
Sujith175 <sujithkumar.sk175@gmail.com>
2022-09-18 20:03:10 +0530

    feat:like and unlike

--- social/src/Screens/Home.jsx
+++ social/src/Screens/Home.jsx
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { UserContext } from "../App";
+
 const Home = () => {
   const [data, setData] = useState([]);
+  const { state, dispatch } = useContext(UserContext);
 
   useEffect(() => {
     fetch("http://localhost:8000/allposts", {
@@ -10,9 +13,64 @@ const Home = () => {
     })
       .then((res) => res.json())
       .then((result) => {
+        console.log(result);
         setData(result.posts);
       });
   }, []);
+
+  const likePost = (id) => {
+    fetch("http://localhost:8000/like", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const unLikePost = (id) => {
+    fetch("http://localhost:8000/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = data.map((item) => {
+          if (item._id == result._id) {
+            return result;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="home">
       {data.map((item) => {
@@ -26,6 +84,29 @@ const Home = () => {
               <i className="material-icons " style={{ color: "red" }}>
                 favorite
               </i>
+              {item.likes.includes(state._id) ? (
+                <i
+                  style={{ cursor: "pointer", paddingLeft: "8px" }}
+                  className="material-icons"
+                  onClick={() => {
+                    unLikePost(item._id);
+                  }}
+                >
+                  thumb_down
+                </i>
+              ) : (
+                <i
+                  style={{ cursor: "pointer", paddingLeft: "8px" }}
+                  className="material-icons"
+                  onClick={() => {
+                    likePost(item._id);
+                  }}
+                >
+                  thumb_up
+                </i>
+              )}
+
+              <h6>{item.likes.length} likes</h6>
               <h6>{item.title}</h6>
               <p>{item.body}</p>
               <input type="text" placeholder="add comment" />
